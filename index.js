6fd943c420203da252bafc906554a849dde3cfeb
goldberg49spear <33773100+goldberg49spear@users.noreply.github.com>
2017-11-22 15:53:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,3 +32,31 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+app.get('/db/getRecords', function(req,res){
+	
+   //res.send(dbOperations.getRecords(req,res));
+	var pg = require('pg');  
+      
+        //You can run command "heroku config" to see what is Database URL from Heroku belt
+      
+        var conString = process.env.DATABASE_URL;
+        var client = new pg.Client(conString);
+        
+        client.connect();
+	var string="select a.Name as accountname,c.Id,firstName,LastName,phone,email from salesforce14.contact c INNER JOIN salesforce14.account a ON a.sfid=c.accountId";
+
+        var query = client.query(string);
+
+        query.on("row", function (row, result) { 
+            result.addRow(row); 
+        });
+
+        query.on("end", function (result) {          
+            client.end();
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(JSON.stringify(result.rows, null, "    ") + "\n");
+            res.end();  
+        });
+	
+});
